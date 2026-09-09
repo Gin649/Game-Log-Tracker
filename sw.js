17def6529dea9f0a035d47d3ec3d74aac1009956
Gin649 <gin649@gmail.com>
2026-09-09 11:10:58 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'gl-tracker-v1';
+const ASSETS_TO_CACHE = [
+'./',
+'./index.html',
+'./manifest.json'
+]; 
+
+// Install Event: Cache interface elements
+self.addEventListener('install', (event) => {
+event.waitUntil(
+caches.open(CACHE_NAME).then((cache) => {
+console.log('Caching tracker shell assets');
+return cache.addAll(ASSETS_TO_CACHE);
+})
+);
+}); 
+
+// Activate Event: Delete older cache versions
+self.addEventListener('activate', (event) => {
+event.waitUntil(
+caches.keys().then((keys) => {
+return Promise.all(
+keys.map((key) => {
+if (key !== CACHE_NAME) {
+return caches.delete(key);
+}
+})
+);
+})
+);
+}); 
+
+// Fetch Event: Serve cached assets offline, fetch network copies when online
+self.addEventListener('fetch', (event) => {
+// Skip caching live data requests to RetroAchievements or RAWG APIs
+if (event.request.url.includes('api')) {
+return;
+} 
+
+event.respondWith(
+caches.match(event.request).then((cachedResponse) => {
+return cachedResponse || fetch(event.request);
+})
+);
+});
