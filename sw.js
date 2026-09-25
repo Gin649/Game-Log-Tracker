491de95aef7f3012f83b1486227d07b3a1a37e17
Gin649 <gin649@gmail.com>
2026-09-24 23:17:32 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 // ==============================================================================
 
 const CACHE_PREFIX = 'gl-tracker-';
-const CACHE_NAME = CACHE_PREFIX + 'v75';
+const CACHE_NAME = CACHE_PREFIX + 'v76';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
